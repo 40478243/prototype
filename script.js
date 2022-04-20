61efa83fc409f3efb9837ea0afe0cdfd9395a1e4
40478243 <95437513+40478243@users.noreply.github.com>
2022-04-21 00:55:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -232,32 +232,18 @@ function myFunction() {
 
 
 
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+let title = document.getElementById("title");
+let navbar = document.getElementById("navbar");
   
-let searchForm = document.querySelector('.search-form');
+let navPos = navbar.getBoundingClientRect().top;
   
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-  
-let cartItem = document.querySelector('.cart-items-container');
-  
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-  
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+window.addEventListener("scroll", e => {
+    let scrollPos = window.scrollY;
+    if (scrollPos > navPos) {
+        navbar.classList.add('sticky');
+        header.classList.add('navbarOffsetMargin');
+    } else {
+        navbar.classList.remove('sticky');
+        header.classList.remove('navbarOffsetMargin');
+    }
+});
