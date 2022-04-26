64111eb376f6057c0782840535a8a1f7683e55c8
40478243 <95437513+40478243@users.noreply.github.com>
2022-04-26 21:36:27 +0100

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,113 +1,112 @@
 // creating an array and passing the number, questions, options, and answers
-let questions = [
+let questions = [{
+        numb: 1,
+        question: "What is capital of Italy?",
+        answer: "Rome",
+        options: [
+            "Rome",
+            "Milan",
+            "Turin",
+            "Venice"
+        ]
+    },
     {
-    numb: 1,
-    question: "What is capital of Italy?",
-    answer: "Rome",
-    options: [
-      "Rome",
-      "Milan",
-      "Turin",
-      "Venice"
-    ]
-  },
+        numb: 2,
+        question: "What is the capital of South Korea?",
+        answer: "Seoul",
+        options: [
+            "Tokyo",
+            "Busan",
+            "Seoul",
+            "Bangkok"
+        ]
+    },
     {
-    numb: 2,
-    question: "What is the capital of South Korea?",
-    answer: "Seoul",
-    options: [
-      "Tokyo",
-      "Busan",
-      "Seoul",
-      "Bangkok"
-    ]
-  },
+        numb: 3,
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            "Nice",
+            "Paris",
+            "Lille",
+            "Lyon"
+        ]
+    },
     {
-    numb: 3,
-    question: "What is the capital of France?",
-    answer: "Paris",
-    options: [
-      "Nice",
-      "Paris",
-      "Lille",
-      "Lyon"
-    ]
-  },
+        numb: 4,
+        question: "What is the capital of Spain?",
+        answer: "Madrid",
+        options: [
+            "Madrid",
+            "Barcelona",
+            "Seville",
+            "Valencia"
+        ]
+    },
     {
-    numb: 4,
-    question: "What is the capital of Spain?",
-    answer: "Madrid",
-    options: [
-      "Madrid",
-      "Barcelona",
-      "Seville",
-      "Valencia"
-    ]
-  },
+        numb: 5,
+        question: "What is the capital of Japan?",
+        answer: "Tokyo",
+        options: [
+            "Kyoto",
+            "Osaka",
+            "Kobe",
+            "Tokyo"
+        ]
+    },
     {
-    numb: 5,
-    question: "What is the capital of Japan?",
-    answer: "Tokyo",
-    options: [
-      "Kyoto",
-      "Osaka",
-      "Kobe",
-      "Tokyo"
-    ]
-  },
-  {
-    numb: 6,
-    question: "What is the capital of the United States?",
-    answer: "Washington D.C",
-    options: [
-      "Los Angeles",
-      "New York City",
-      "Washington D.C",
-      "Miami"
-    ]
-  },
-  {
-    numb: 7,
-    question: "What is the capital of Egypt?",
-    answer: "Cairo",
-    options: [
-      "Alexandria",
-      "Algiers",
-      "Sharm el Sheikh",
-      "Cairo"
-    ]
-  },
-  {
-    numb: 8,
-    question: "What is the capital of Scotland?",
-    answer: "Edinburgh",
-    options: [
-      "Edinburgh",
-      "Glasgow",
-      "Perth",
-      "Aberdeen"
-    ]
-  },
-  {
-    numb: 9,
-    question: "What is the capital of Australia?",
-    answer: "Canberra",
-    options: [
-      "Sydney",
-      "Canberra",
-      "Melbourne",
-      "Perth"
-    ]
-  },
-  {
-    numb: 10,
-    question: "What is the capital of Argentina?",
-    answer: "Buenos Aires",
-    options: [
-      "Buenos Aires",
-      "Rio di Janiero",
-      "Mendoza",
-      "Bogota"
-    ]
-  },
+        numb: 6,
+        question: "What is the capital of the United States?",
+        answer: "Washington D.C",
+        options: [
+            "Los Angeles",
+            "New York City",
+            "Washington D.C",
+            "Miami"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the capital of Egypt?",
+        answer: "Cairo",
+        options: [
+            "Alexandria",
+            "Algiers",
+            "Sharm el Sheikh",
+            "Cairo"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the capital of Scotland?",
+        answer: "Edinburgh",
+        options: [
+            "Edinburgh",
+            "Glasgow",
+            "Perth",
+            "Aberdeen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the capital of Australia?",
+        answer: "Canberra",
+        options: [
+            "Sydney",
+            "Canberra",
+            "Melbourne",
+            "Perth"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the capital of Argentina?",
+        answer: "Buenos Aires",
+        options: [
+            "Buenos Aires",
+            "Rio di Janiero",
+            "Mendoza",
+            "Bogota"
+        ]
+    },
 ];
