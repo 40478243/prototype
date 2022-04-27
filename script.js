3c04448584e125d8637ef1124fc6732ffb7c537a
40478243 <95437513+40478243@users.noreply.github.com>
2022-04-27 23:17:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //selecting all required elements
-const starting_btn = document.querySelector(".starting_btn button");
+const starting_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restarting");
+const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
-const restarting_quiz = result_box.querySelector(".buttons .restarting");
+const restarting_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 const next_btn = document.querySelector("footer .next_btn");
 const bottom_question_counter = document.querySelector("footer .total_question");
