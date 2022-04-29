f83dbd3d74af0481513fbee730231dac459cccba
40478243 <95437513+40478243@users.noreply.github.com>
2022-04-29 01:37:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //all of the elements required are being selected below
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
 const timer_line = document.querySelector("header .timer_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
@@ -16,12 +16,12 @@ const bottom_question_counter = document.querySelector("footer .total_question")
 
 // setting other variables
 let timeValue = 30;
-let question_count = 0;
-let question_number = 1;
-let userScore = 0;
 let counter;
 let counterLine;
 let widthValue = 0;
+let question_count = 0;
+let question_number = 1;
+let userScore = 0;
 
 // the sounds downloaded for the quiz are selected here
 var soundCorrect = new Audio("rightsound.mp3");
@@ -61,10 +61,10 @@ restart_quiz.onclick = () => {
     quiz_box.classList.add("activeQuiz");
     result_box.classList.remove("activeResult");
     timeValue = 30;
-    question_count = 0;
-    question_number = 1;
     userScore = 0;
     widthValue = 0;
+    question_count = 0;
+    question_number = 1;
     f2();
 }
 next_btn.onclick = () => {
@@ -101,15 +101,15 @@ function showQuetions(index) {
 function optionSelected(answer) {
     clearInterval(counter);
     clearInterval(counterLine);
-    let userAns = answer.textContent;
-    let correcAns = questions[question_count].answer;
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[question_count].answer;
     const allOptions = option_list.children.length;
 
     // adding the icons for the tick and cross
     let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
     let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
     //what will happen if the user selects right or wrong answers
-    if (userAns == correcAns) {
+    if (userAnswer == correctAnswer) {
         userScore += 1;
         answer.classList.add("correct");
         soundCorrect.play();
@@ -123,7 +123,7 @@ function optionSelected(answer) {
         console.log("Wrong Answer");
 
         for (i = 0; i < allOptions; i++) {
-            if (option_list.children[i].textContent == correcAns) {
+            if (option_list.children[i].textContent == correctAnswer) {
                 option_list.children[i].setAttribute("class", "option correct");
                 option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
                 console.log("Auto selected correct answer.");
@@ -167,18 +167,18 @@ function startTimer(time) {
 
 //question counter at bottom of the page using span
 function questionCounter(index) {
-    let totalQuestionCounTag = '<span><p>' + index + '</p> of <p>' + questions.length + '</p></span>';
+    let totalQuestionCounTag = '<span><p>' + index + '</p> of <p>10</p></span>';
     bottom_question_counter.innerHTML = totalQuestionCounTag;
 }
 
 window.onscroll = () => {
     let navbarLinks = document.querySelectorAll("nav a");
 
-    scrollpos = window.scrollY;
+    scrollposition = window.scrollY;
     navbarLinks.forEach(link => {
         let section = document.querySelector(link.hash);
-        if (section.offsetTop <= scrollpos + 150 &&
-            section.offsetTop + section.offsetHeight > scrollpos + 150) {
+        if (section.offsetTop <= scrollposition + 155 &&
+            section.offsetTop + section.offsetHeight > scrollposition + 155) {
             link.classList.add("active");
         } else {
             link.classList.remove("active");
